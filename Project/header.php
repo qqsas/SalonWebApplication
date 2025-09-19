9b753f03140a07fde16a586bb40b0e4e142153b7
qqsas <eugene.zippy@gmail.com>
2025-09-19 15:13:30 +0200

    header done, needs css

--- Project/header.php
+++ Project/header.php
@@ -22,43 +22,42 @@ if (isset($_SESSION['user_id'])) {
 ?>
 
 <!-- Header -->
-<nav class="custom-navbar">
+<nav class="Header-navbar">
     <div class="container-fluid">
         <a class="navbar-brand" href="homepage.php">
-            <img src="images/logo.png" alt="Logo" style="height: 30px; margin-right: 10px;">
-            E-Commerce Platform
-            
-
+            <img src="Img/logo.jpeg" alt="Logo" style="height: 30px; margin-right: 10px;">
+            Kumar Kailey Hair & Beauty Salon
         </a>
-<button class="menu-toggle" onclick="document.querySelector('.custom-navbar').classList.toggle('active')">
+<button class="menu-toggle" onclick="document.querySelector('.Header-navbar').classList.toggle('active')">
     ☰
 </button>
         
-        <div class="collapse navbar-collapse" id="navbarNav">
+        <div class="HeaderNavBody" id="navbarNav">
             
             <ul class="navbar-nav ms-auto">
-                <li class="nav-item"><a class="nav-link" href="support.php">Support</a></li>
+                <li class="nav-item"><a class="nav-link" href="support.php">Contact</a></li>
                 <li class="nav-item"><a class="nav-link" href="about.php">About</a></li>
-                <li class="nav-item"><a class="nav-link" href="products.php">All Products</a></li>
-                <li class="nav-item"><a class="nav-link" href="specials.php">Specials</a></li>
+                <li class="nav-item"><a class="nav-link" href="products.php">Services</a></li>
+                <li class="nav-item"><a class="nav-link" href="specials.php">Products</a></li>
+                <li class="nav-item"><a class="nav-link" href="specials.php">Staff</a></li>
                 <li class="nav-item"><a class="nav-link" href="homepage.php">Home</a></li>
 
-                <?php if (isset($_SESSION['user_id'])): ?>
-                    <?php if ($user_role === 'admin'): ?>
+                <?php if (isset($_SESSION['UserID'])): ?>
+                    <?php if ($user_role === 'Admin'): ?>
                         <li class="nav-item"><a class="nav-link" href="admin_dashboard.php">Admin Dashboard</a></li>
-                    <?php elseif ($user_role === 'seller'): ?>
+                    <?php elseif ($user_role === 'Barber'): ?>
                         <li class="nav-item"><a class="nav-link" href="seller_dashboard.php">Dashboard</a></li>
-                    <?php elseif ($user_role === 'buyer'): ?>
+                    <?php elseif ($user_role === 'Customer'): ?>
                         <li class="nav-item">
-                            <a class="nav-link d-flex align-items-center" href="cart.php">
-                                <i class="bi bi-cart3 fs-5 me-1"></i>
+                            <a class="CartWrapper" href="cart.php">
+                                <i class="CartBody"></i>
                                 <span>🛒 Cart</span>
                                 <?php if ($cartItemCount > 0): ?>
                                     <span class="cart-number"><?= $cartItemCount ?></span>
                                 <?php endif; ?>
                             </a>
                         </li>
-                        <li class="nav-item"><a class="nav-link" href="buyer_orders.php">My Orders</a></li>
+                        <li class="nav-item"><a class="nav-link" href="buyer_orders.php">Orders</a></li>
                     <?php endif; ?>
                     <li class="nav-item"><a class="nav-link" href="logout.php">Logout</a></li>
                 <?php else: ?>
