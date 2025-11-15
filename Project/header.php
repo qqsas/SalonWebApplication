5844e62d33b8346fa1654dc8e96e0d51e85b45f9
qqsas <eugene.zippy@gmail.com>
2025-11-15 14:41:17 +0200

    grey text and navbar spacing fix

--- Project/header.php
+++ Project/header.php
@@ -72,7 +72,7 @@ if (isset($_SESSION['UserID'])) {
         </button>
         
         <div class="HeaderNavBody" id="navbarNav">
-            <ul class="navbar-nav ms-auto">
+            <ul class="navbar-nav">
                 <li class="nav-item"><a class="nav-link" href="index.php">Home</a></li>
                 <li class="nav-item"><a class="nav-link" href="about.php">About</a></li>
                 <li class="nav-item"><a class="nav-link" href="services.php">Services</a></li>
